c4984cfb5fc118fee4e1fd2f8d6329d02f8a2346
lukebev06 <lmb73@kent.ac.uk>
2026-01-20 02:02:24 +0000

    done

--- partThree.py
+++ partThree.py
@@ -1,14 +1,23 @@
 def main():
     pounds = pounds_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to charge? "))
+
     charge = pounds * percent
+
     print(f"Charge £{charge:.2f}")
+    #only prints the tip not total
 
 
 def pounds_to_float(d):
-    # TODO
+
+    d = d.replace("£","")
+    return float(d)
 
 def percent_to_float(p):
-    # TODO
+
+    p = p.replace("%","")
+    p = float(p)
+
+    return(p/100)
 
 main()
--- partTwo.py
+++ partTwo.py
@@ -1,9 +1,15 @@
 import math  
 
 def main():
-#TO DO  
+
+    A = int(input("write the first number: "))
+    B = int(input("write the second number: "))
+
+    C = pythag(A, B)
+    print(f"The length of the hypotenuse is {C}")
 
 def pythag(A,B):
-#TO DO  
+
+    return math.sqrt(A**2 +B**2)
 
 main()
